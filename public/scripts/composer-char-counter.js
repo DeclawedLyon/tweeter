6ee846570121153a66d8f057740a91b3e57fd7ec
DeclawedLyon <declan.e.lyons@gmail.com>
2021-08-12 14:49:52 -0600

    Created a character counter that changes red once a limit is surpassed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,25 @@
-console.log('EEEYYYYYYYY')
-
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log("it works")
+  console.log(jQuery)
+  const $textInput = $('#input-field' );
+  const $charCounter = $('.char-count');
+  const $submitTweet = $('#submit-tweet')
+  // console.log($charCounter)
+  let count = 128;
+  $textInput.on('keypress', (event) => {
+    // console.log(event);
+    count--;
+    if (count < 0) {
+      $charCounter.addClass('danger')
+      $charCounter.removeClass('counter')
+    }
+    console.log("charcounter is: ", $charCounter.val())
+    $charCounter.val(count)
+    // console.log($textInput.val())
+    
+  })
+  $submitTweet.on('click', () => {
+    const newVal = $textInput.val();
+    console.log(newVal);
+  })
 });
