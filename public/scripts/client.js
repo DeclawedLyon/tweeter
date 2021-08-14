ed134f177d0c123949f43d17a4488fb45eaee23b
DeclawedLyon <declan.e.lyons@gmail.com>
2021-08-14 16:06:53 -0600

    Added post and load tweets functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,80 +4,46 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-// takes a database and a username and returns an object
-// const getTweetData = (dataBase, tweetName) => {
-//   for (const object of dataBase) {
-//     if (object.user.name === tweetName) {
-//       const tweetObject = {
-//         userName: object.user.name,
-//         userAvatar: object.user.avatars,
-//         userHandle: object.user.handle,
-//         tweetContent: object.content.text,
-//         createdAt: object.created_at
-//       }
-//       return tweetObject;
-//     }
-//   }
-// }
-
 $(document).ready(function () {
-  const $button = $('#submit-tweet');
-  const $newTweetInput = $('#input-field');
-  const $articleText = $('.article-text > p');
   const $contentContainer = $("#content-container");
 
-  // Fake data taken from initial-tweets.json
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": '<i class="fas fa-user-astronaut"></i>',
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": '<i class="fas fa-user-astronaut"></i>',
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  $button.on('click', () => {
-    const textVal = $newTweetInput.val();
-    console.log("The text val is:", textVal);
-  });
+  const loadTweets = () => {
+    // renderTweets(tweetData);
 
-  const createTweetElement = function (tweet) {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      datatype: 'json',
+      success: (posts) => {
+        renderTweets(posts);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
 
+  const createTweetElement = function (tweetObject) {
+    let date = timeago.format(tweetObject.createdAt);
     let $tweet = `<div id="tweets-container">
     <article id="old-tweet-text">
       <header>
         <div class="avatar-location">
-          ${tweet.userAvatar}
-          <span>${tweet.userName}</span>
+          ${tweetObject.userAvatar}
+          <span>${tweetObject.userName}</span>
         </div>
         <div>
           <i class="fas fa-at"></i>
-          <span>${tweet.userHandle}</span>
+          <span>${tweetObject.userHandle}</span>
         </div>
       </header>
       <footer class="article-text">
-        <p>${tweet.tweetContent}</p>
+        <p>${tweetObject.tweetContent}</p>
       </footer>
     </article>
     <div>
       <div id="engagement-container">
-        <span>${tweet.createdAt}</span>
+        <span>${date}</span>
       </div>
       <div class="engagement-buttons">
           <i class="fas fa-flag"></i>
@@ -94,6 +60,7 @@ $(document).ready(function () {
   // const $tweet = $(`<article class="tweet">Hello world</article>`);
   const renderTweets = function (tweets) {
     // loops through tweets
+    $contentContainer.empty();
     for (const object of tweets) {
       // console.log("the tweets[object] is:", tweets[object]);
       const tweetObject = {
@@ -109,5 +76,18 @@ $(document).ready(function () {
       $contentContainer.append(newTweetTemplate)
     }
   }
-renderTweets(tweetData);
+
+  loadTweets();
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    const user = "james";
+    const $tweetInput = $('tweet-text');
+    console.log($(this))
+    const urlEncoded = $(this).serialize();
+    console.log("---", urlEncoded)
+
+    $.post('/tweets',urlEncoded).then(loadTweets);
+
+  });
 })
